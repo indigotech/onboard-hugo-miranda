bc4c150eaedc4047b04b74ca06d1bf2d7972490c
hugo miranda <hugo.miranda.souza@gmail.com>
2021-01-05 14:19:41 -0300

    Setup Typescript w/ Apollo Server

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import { ApolloServer, gql } from 'apollo-server';
+
+const typeDefs = gql`
+  type Query {
+    hello: String,
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'hello, world!',
+  },
+}
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => console.log(`server is running on ${url}`))
