67b44e505c24ad4b54b8044d3f8d543081b3adff
hugo miranda <hugo.miranda.souza@gmail.com>
2021-01-08 15:37:36 -0300

    Create login mutation

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,45 @@ import 'reflect-metadata';
 import './typeorm';
 import { ApolloServer, gql } from 'apollo-server';
 
+const fakeUsers = [
+  {
+    id: '1',
+    name: 'username',
+    email: 'email@mail.com',
+    password: 'password',
+    birthdate: '04/01/2001',
+    cpf: '12345678901',
+  },
+];
+
 const typeDefs = gql`
+  type User {
+    id: String
+    name: String
+    email: String
+    password: String
+    birthdate: String
+    cpf: String
+  }
+
+  type LoginResponse {
+    user: User!
+    token: String!
+  }
+
   type Query {
     hello: String
+  }
+
+  type Mutation {
+    login(login: LoginInput): LoginResponse!
+  }
+
+  input LoginInput {
+    "User email"
+    email: String!
+    "User password"
+    password: String!
   }
 `;
 
@@ -12,6 +48,23 @@ const resolvers = {
   Query: {
     hello: () => {
       return 'hello, world!';
+    },
+  },
+
+  Mutation: {
+    login: (_, { login }) => {
+      const { email, password } = login;
+
+      const user = fakeUsers.filter((user) => user.email === email && user.password === password)[0];
+      const token = 'Token';
+
+      if (!user) {
+        throw new Error('Unauthorized. Possible invalid credentials.');
+      }
+
+      const response = { user, token };
+
+      return response;
     },
   },
 };
