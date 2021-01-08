04532cccb6e8a24ab8db2aef47a3428b5e3d066f
hugo miranda <hugo.miranda.souza@gmail.com>
2021-01-08 10:21:19 -0300

    Update database connection

--- src/typeorm/index.ts
+++ src/typeorm/index.ts
@@ -1,9 +1,8 @@
 import { createConnection } from 'typeorm';
 
-try {
-  createConnection();
-  console.log('Database connection was successful!');
-} catch (error) {
-  console.log(`Something went wrong with database connection
-  \n ${error?.message}`);
-}
+createConnection()
+  .then(() => console.log(`Database connection was successful!`))
+  .catch((error) => {
+    console.log(`Something went wrong with database connection
+    \n${error?.message}`);
+  });
