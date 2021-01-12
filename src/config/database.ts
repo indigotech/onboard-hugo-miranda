b1cc71ed978901a9b1443b6f4f412e4def631401
hugo miranda <hugo.miranda.souza@gmail.com>
2021-01-12 09:16:34 -0300

    fix env var DATABASE_URL, remove DATABASE_PORT

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,7 +4,6 @@ export const DatabaseConfig: PostgresConnectionOptions = {
   name: 'default',
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  port: Number(process.env.DATABASE_PORT),
   entities: ['./src/typeorm/entities/*/*-entity.ts'],
   migrations: ['./src/typeorm/migrations/*.ts'],
 };
