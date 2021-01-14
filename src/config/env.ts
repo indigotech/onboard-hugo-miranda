39070c91fe44923ac6f52c8be3200fb6d18b4f84
hugo miranda <hugo.miranda.souza@gmail.com>
2021-01-14 15:36:36 -0300

    fix & organize env vars

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,18 +1,16 @@
 import * as dotenv from 'dotenv';
 import path from 'path';
 
-export const SelectedEnv = process.env.ENV || 'DEFAULT';
+export const SelectedEnv = process.env.ENV || 'DEV';
 
 export enum Environments {
   TEST = 'TEST',
   DEV = 'DEV',
-  DEFAULT = 'DEFAULT',
 }
 
 const envPaths = {
   [Environments.TEST]: path.resolve(__dirname, '..', '..', '.test.env'),
   [Environments.DEV]: path.resolve(__dirname, '..', '..', '.dev.env'),
-  [Environments.DEFAULT]: path.resolve(__dirname, '..', '..', '.env'),
 };
 
 dotenv.config({ path: envPaths[SelectedEnv] });
