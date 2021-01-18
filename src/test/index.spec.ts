08dd3ae83fe20cc98139d2fe68af3efe7895cd22
hugo miranda <hugo.miranda.souza@gmail.com>
2021-01-18 19:24:18 -0300

    tests: fix chain & add tests for token expiration

--- src/test/index.spec.ts
+++ src/test/index.spec.ts
@@ -67,12 +67,14 @@ describe('E2E GraphQL - Mutation - User', () => {
             cpf
           }
           token
+          rememberMe
         }
       }`,
       variables: {
         input: {
           email: 'useremail1',
           password: 'userpassword1',
+          rememberMe: false,
         },
       },
     };
@@ -82,23 +84,159 @@ describe('E2E GraphQL - Mutation - User', () => {
 
     const verifiedToken = await jwtProvider.verify(response.token);
 
-    expect(response)
-      .to.have.property('user')
-      .which.is.a('object')
-      .that.has.keys(['id', 'name', 'email', 'cpf', 'birthDate'])
-      .which.include({
-        id: users[0].id.toString(),
-        name: 'username1',
-        email: 'useremail1',
-        birthDate: 'userbirthDate1',
-        cpf: 'usercpf1',
-      });
+    expect(response).to.have.property('user').to.be.deep.eq({
+      id: users[0].id.toString(),
+      name: 'username1',
+      email: 'useremail1',
+      birthDate: 'userbirthDate1',
+      cpf: 'usercpf1',
+    });
 
     expect(response).to.have.property('token').which.is.a('string');
-    expect(verifiedToken)
-      .to.have.property('payload')
-      .which.is.a('object')
-      .that.have.key('userId')
-      .that.is.eql({ userId: users[0].id });
+    expect(verifiedToken.payload.userId).to.be.eq(users[0].id);
+  });
+
+  it('Should validate jwt token after expiration time (not remember me)', async () => {
+    const request = {
+      query: `mutation login ($input: LoginInput! ) {
+        login( input: $input){
+          user {
+            id
+            name
+            email
+            birthDate
+            cpf
+          }
+          token
+          rememberMe
+        }
+      }`,
+      variables: {
+        input: {
+          email: 'useremail1',
+          password: 'userpassword1',
+          rememberMe: false,
+        },
+      },
+    };
+
+    const { body } = await supertest(baseURL).post('').send(request);
+    const response = body.data.login;
+
+    const verifiedToken = await jwtProvider.verify(response.token);
+
+    expect(response).to.have.property('user').to.be.deep.eq({
+      id: users[0].id.toString(),
+      name: 'username1',
+      email: 'useremail1',
+      birthDate: 'userbirthDate1',
+      cpf: 'usercpf1',
+    });
+
+    const currentTimestamp = new Date();
+    currentTimestamp.setMilliseconds(0);
+
+    const expectExpiration = currentTimestamp;
+    expectExpiration.setMinutes(currentTimestamp.getMinutes() + 15);
+
+    const tokenExpires = new Date(verifiedToken.exp * 1000);
+
+    const expiresIn15m = +expectExpiration === +tokenExpires;
+    const isTokenExpired = currentTimestamp > tokenExpires;
+
+    expect(isTokenExpired).to.be.eq(false);
+    expect(expiresIn15m).to.be.eq(true);
+  });
+
+  it('Should validate jwt token after expiration time (remember me)', async () => {
+    const request = {
+      query: `mutation login ($input: LoginInput! ) {
+        login( input: $input){
+          user {
+            id
+            name
+            email
+            birthDate
+            cpf
+          }
+          token
+          rememberMe
+        }
+      }`,
+      variables: {
+        input: {
+          email: 'useremail1',
+          password: 'userpassword1',
+          rememberMe: true,
+        },
+      },
+    };
+
+    const { body } = await supertest(baseURL).post('').send(request);
+    const response = body.data.login;
+
+    const verifiedToken = await jwtProvider.verify(response.token);
+
+    expect(response).to.have.property('user').to.be.deep.eq({
+      id: users[0].id.toString(),
+      name: 'username1',
+      email: 'useremail1',
+      birthDate: 'userbirthDate1',
+      cpf: 'usercpf1',
+    });
+
+    const currentTimestamp = new Date();
+    currentTimestamp.setMilliseconds(0);
+
+    const expectExpiration = currentTimestamp;
+    expectExpiration.setMinutes(currentTimestamp.getMinutes() + 30);
+
+    const tokenExpires = new Date(verifiedToken.exp * 1000);
+
+    const expiresIn15m = +expectExpiration === +tokenExpires;
+    const isTokenExpired = currentTimestamp > tokenExpires;
+
+    expect(isTokenExpired).to.be.eq(false);
+    expect(expiresIn15m).to.be.eq(true);
+  });
+
+  it('Should not validate jwt token after expiration time', async () => {
+    const request = {
+      query: `mutation login ($input: LoginInput! ) {
+        login( input: $input){
+          user {
+            id
+            name
+            email
+            birthDate
+            cpf
+          }
+          token
+          rememberMe
+        }
+      }`,
+      variables: {
+        input: {
+          email: 'useremail1',
+          password: 'userpassword1',
+          rememberMe: false,
+        },
+      },
+    };
+
+    const { body } = await supertest(baseURL).post('').send(request);
+    const response = body.data.login;
+
+    const verifiedToken = await jwtProvider.verify(response.token);
+
+    const tokenExpires = new Date(verifiedToken.exp * 1000);
+    const afterExpires = new Date();
+    afterExpires.setMilliseconds(0);
+
+    const expectExpiration = afterExpires;
+    expectExpiration.setMinutes(afterExpires.getMinutes() - 1);
+    const isTokenExpired = tokenExpires > afterExpires;
+
+    expect(isTokenExpired).to.be.eql(true);
   });
 });
