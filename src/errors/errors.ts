37136f6001d7b18bbeca176d92dc4640d6d1222b
hugo miranda <hugo.miranda.souza@gmail.com>
2021-01-18 15:49:18 -0300

    errors: custom class to handle errors

---
+++ src/errors/errors.ts
@@ -0,0 +1,19 @@
+export interface IAppError extends Error {
+  code: number;
+  message: string;
+  additionalInfo?: string;
+}
+
+export class AppError extends Error implements IAppError {
+  public readonly message: string;
+  public readonly code: number;
+  public readonly additionalInfo: string;
+
+  constructor(message: string, code = 400, additionalInfo?: string) {
+    super(message);
+
+    this.message = message;
+    this.code = code;
+    this.additionalInfo = additionalInfo;
+  }
+}
