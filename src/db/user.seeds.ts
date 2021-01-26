172fcc12ed92e64131030d3f639a56744115c643
hugo miranda <hugo.miranda.souza@gmail.com>
2021-01-26 16:31:41 -0300

    create file to run the script seeds

--- src/db/user.seeds.ts
+++ src/db/user.seeds.ts
@@ -1,19 +1,20 @@
-import { connectDB } from 'src/typeorm';
 import faker from 'faker';
 import { getRepository } from 'typeorm';
 import { User } from 'src/typeorm/entities/users/user-entity';
+import { HashProvider } from 'src/typeorm/entities/users/providers/hash-provider/hash-provider';
 
 export async function userSeeds(numOfUsersToAdd = 50): Promise<void> {
   try {
-    await connectDB();
     const users: User[] = [];
+    const hashProvider = new HashProvider();
 
     const usersRepository = getRepository(User);
     for (let i = 0; i < numOfUsersToAdd; i++) {
+      const password = await hashProvider.generate(`passw0rd`);
       const userData = {
         name: faker.name.findName(),
         email: faker.internet.email(),
-        password: `passw0rd`,
+        password,
         cpf: `12345678900`,
         birthDate: `01/01/2000`,
       };
@@ -27,5 +28,3 @@ export async function userSeeds(numOfUsersToAdd = 50): Promise<void> {
     console.log(error);
   }
 }
-
-userSeeds().catch((error) => console.log(error));
--- src/typeorm/index.ts
+++ src/typeorm/index.ts
@@ -1,10 +1,11 @@
-import { createConnection } from 'typeorm';
+import { Connection, createConnection } from 'typeorm';
 import { DatabaseConfig } from '@config/database';
 
-export async function connectDB(): Promise<void> {
+export async function connectDB(): Promise<Connection> {
   try {
     const connection = await createConnection(DatabaseConfig);
     console.log(`Database connection to "${connection.driver.database}" was successful!`);
+    return connection;
   } catch (error) {
     console.log(`Something went wrong with database connection
     \n${error?.message}`);
